fa55ac5a33314f6554488debd2ec03e9d6f21654
Rahul Kanawade <kanwader7@gmail.com>
2025-09-29 12:17:43 +0530

    Backend ready for deployment

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.laxmi.retailmatrix',
+  appName: 'retailmatrix',
+  webDir: 'build'
+};
+
+export default config;
