701e9bd8d14f38338f448f18f7ba46b34b097a80
Rahul Kanawade <kanwader7@gmail.com>
2025-10-07 14:33:27 +0530

    some changes

--- backend/backend.js
+++ backend/backend.js
@@ -39,7 +39,7 @@ const pool = mysql.createPool({
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT,
-  ssl: { rejectUnauthorized: true }
+  ssl: { rejectUnauthorized: false }
 });
 
 const validator = require('validator');
